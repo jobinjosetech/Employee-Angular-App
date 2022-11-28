04d30e401266aae85ae67d22b643595427d17463
jobin <jobinjose.in@gmail.com>
2022-11-28 12:39:53 +0530

    read values

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
+  code=""
+  name=""
+  designation=""
+  salary=""
+  phone=""
+  email=""
+  company=""
+  experience=""
 
+  readValues=()=>{
+
+    let employees = {
+      "code":this.code,
+      "name":this.name,
+      "designation":this.designation,
+      "salary":this.salary,
+      "phone":this.salary,
+      "email":this.email,
+      "company":this.company,
+      "experience":this.experience
+
+    }
+    console.log(employees)
+  }
 }
--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-employee.component.css']
 })
 export class ViewEmployeeComponent {
-
+  data:any = [
+    {
+      "name":"Jobin",
+      "designation":"Software Engineer Trainee",
+      "Salary":25000,
+      "Company":"Nest",
+    },
+    {
+      "name":"Joel",
+      "designation":"Software Engineer",
+      "Salary":35000,
+      "Company":"UST"
+    },
+    {
+      "name":"Yethi",
+      "designation":"App Developer",
+      "Salary":48000,
+      "Company":"TCS"
+    },
+    {
+      "name":"Vishnu",
+      "designation":"Software Engineer Trainee",
+      "Salary":25000,
+      "Company":"Nest"
+    },
+    {
+      "name":"Yedu",
+      "designation":"System Engineer",
+      "Salary":28000,
+      "Company":"Infosys"
+    },
+    {
+      "name":"Kiran",
+      "designation":"System Engineer Trainee",
+      "Salary":38000,
+      "Company":"Infosys"
+    },
+    {
+      "name":"Yedu",
+      "designation":"System Engineer",
+      "Salary":28000,
+      "Company":"Infosys"
+    }
+  ]
 }
